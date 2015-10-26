2e0e26324ee551869a871f3f2b32086b8e01c2da
Kelly Dunn <kellydunn@simple.com>
2015-10-26 00:04:10 -0700

    css,server: Adding in skeleton css for some basic framing

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express'), app = express();
 
 app.use('/', express.static(__dirname + '/app'));
+app.use("/css", express.static(__dirname + '/css'));
 app.listen(process.env.PORT || 3000);
